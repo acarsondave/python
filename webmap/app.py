ef87c8fe962e5c332afb752e84d638addb865662
Acarson Dave <acarsondave7@gmail.com>
2020-06-30 16:32:07 +0100

    finalized version

--- webmap/app.py
+++ webmap/app.py
@@ -19,10 +19,18 @@ def colorize(elvation_pass):
         return "red"
 
 map1 = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles='Stamen Terrain')
-fg = folium.FeatureGroup(name="my Map")
+fgvolc = folium.FeatureGroup(name="Volcanoes")
 for lati, long, elev, name in zip(lat,lon,el,name):
     iFrame = folium.IFrame(html=html % (name, name, elev), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lati, long], popup=folium.Popup(iFrame), icon=folium.Icon(color=colorize(elev))))
-map1.add_child(fg)
+    fgvolc.add_child(folium.Marker(location=[lati, long], popup=folium.Popup(iFrame), icon=folium.Icon(color=colorize(elev))))
+
+fgpop = folium.FeatureGroup(name="Population")
+fgpop.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map1.add_child(fgvolc)
+map1.add_child(fgpop)
+map1.add_child(folium.LayerControl())
 
 map1.save('map1.html')
